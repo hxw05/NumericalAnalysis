3ecffeffbe0c7b658dae8c1db18a718132182017
hxw05 <hxw05@whut.edu.cn>
2024-12-01 14:28:46 +0800

    算法修改+测试

--- Algorithms.h
+++ Algorithms.h
@@ -33,14 +33,12 @@ double CompositeSimpson(double a, double b, int n, double (*f)(double)) {
     double h = (b - a) / n;
     double s = 0;
     double x = a;
+
     for (int k = 0; k <= n - 1; k++) {
-        cout << "+f x[" << k << "]" << endl;
         s += f(x);
         x += h / 2;
-        cout << "+4f x[" << k << "+1/2]" << endl;
         s += 4 * f(x);
         x += h / 2;
-        cout << "+f x[" << k  + 1 << "]" << endl;
         s += f(x);
     }
 
@@ -48,7 +46,9 @@ double CompositeSimpson(double a, double b, int n, double (*f)(double)) {
     return s;
 }
 
-double VariableStepRectangle(double a, double b, double e, double(*f)(double)) {
+std::vector<double> VariableStepRectangle(double a, double b, double e, double(*f)(double)) {
+    std::vector<double> result;
+
     double h = b - a;
     double s, x, t2;
     double t1 = (h / 2) * (f(a) + f(b));
@@ -59,16 +59,51 @@ double VariableStepRectangle(double a, double b, double e, double(*f)(double)) {
             s += f(x);
             x += h;
         }
+
         t2 = (t1 / 2) + (h / 2) * s;
 
-        if (abs(t2 - t1) < e) return t2;
+        result.push_back(t2);
+
+        if (abs(t2 - t1) < e) return result;
 
         h /= 2;
         t1 = t2;
     }
 }
 
-double Romberg(double a, double b, double e, double(*f)(double)) {
+std::vector<double> FixedStepRectangle(double a, double b, int step, double(*f)(double)) {
+    std::vector<double> res;
+
+    double h = b - a;
+    double s, x, t2;
+    double t1 = (h / 2) * (f(a) + f(b));
+    for (int i = 1; i <= step; i++) {
+        s = 0;
+        x = a + (h / 2);
+        while (x < b) {
+            s += f(x);
+            x += h;
+        }
+
+        t2 = (t1 / 2) + (h / 2) * s;
+
+        res.push_back(t2);
+
+        h /= 2;
+        t1 = t2;
+    }
+
+    return res;
+}
+
+typedef struct RombergResult {
+    std::vector<double> values;
+    int step1 = 0; // 梯形公式的二分次数
+} RombergResult;
+
+RombergResult Romberg(double a, double b, double e, double(*f)(double)) {
+    RombergResult result;
+
     double h = b - a;
     double t1 = (h / 2) * (f(a) + f(b));
     int k = 1;
@@ -76,20 +111,24 @@ double Romberg(double a, double b, double e, double(*f)(double)) {
     while (true) {
         s = 0;
         x = a + (h / 2);
-        s += f(x);
-        x += h;
 
-        if (x < b) continue;
+        while (true) {
+            s += f(x);
+            x += h;
+            if (x >= b) break;
+        }
 
         t2 = (t1 / 2) + (h / 2) * s;
+        result.step1++;
         s2 = t2 + (t2 - t1) / 3;
 
-        if (k != 1) {
+        if (k > 1) {
             c2 = s2 + (s2 - s1) / 15;
-            if (k != 2) {
+            if (k > 2) {
                 r2 = c2 + (c2 - c1) / 63;
-                if (k != 3) {
-                    if (abs(r2 - r1) < e) return r2;
+                if (k > 3) {
+                    result.values.push_back(r2);
+                    if (abs(r2 - r1) < e) return result;
                     else goto K3;
                 } else goto K3;
             } else goto K2;
@@ -109,78 +148,110 @@ double Romberg(double a, double b, double e, double(*f)(double)) {
     }
 }
 
-std::pair<double, double> ImprovedEuler(double x0, double y0, double h, int N, double(*f)(double, double)) {
+std::vector<std::pair<double, double>>
+ImprovedEuler(double x0, double y0, double h, int N, double(*f)(double, double)) {
+    std::vector<std::pair<double, double>> result;
+
     int n = 1;
     double x1, y1, yp, yc;
-    while (n < N) {
+    while (n <= N) {
         x1 = x0 + h;
         yp = y0 + h * f(x0, y0);
         yc = y0 + h * f(x1, yp);
         y1 = (yp + yc) / 2;
+
+        result.push_back({x1, y1});
+
         n++;
         x0 = x1;
         y0 = y1;
     }
 
-    return {x1, y1};
+    return result;
 }
 
-std::pair<double, double> RungeKutta4th(double x0, double y0, double h, int N, double(*f)(double, double)) {
+/**
+ * 利用四阶龙格-库塔格式（经典格式）计算初值问题
+ * @param x0
+ * @param y0
+ * @param h
+ * @param N
+ * @param f
+ * @return
+ */
+std::vector<std::pair<double, double>>
+RungeKutta4th(double x0, double y0, double h, int N, double(*f)(double, double)) {
+    std::vector<std::pair<double, double>> result;
+
     int n = 1;
     double x1, y1, k1, k2, k3, k4;
-    while (n < N) {
+    while (n <= N) {
         x1 = x0 + h;
         k1 = f(x0, y0);
         k2 = f(x0 + (h / 2), y0 + (h / 2) * k1);
         k3 = f(x0 + (h / 2), y0 + (h / 2) * k2);
         k4 = f(x1, y0 + h * k3);
-        y1 = h * (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+        y1 = h * (k1 + 2 * k2 + 2 * k3 + k4) / 6 + y0;
+
+        result.push_back({x1, y1});
+
         n++;
         x0 = x1;
         y0 = y1;
     }
-    return {x1, y1};
+    return result;
 }
 
-std::vector<std::pair<double, double>>
-AdamsPredictorCorrector(double x0, double y0, double h, int N, double(*f)(double, double)) {
+typedef struct AdamsResult {
+    std::vector<std::pair<double, double>> predictors;
+    std::vector<std::pair<double, double>> correctors;
+} AdamsResult;
+
+AdamsResult
+AdamsPredictorCorrectorBasedOnRK4(double x0, double y0, double h, int N, double(*f)(double, double)) {
     double y[5];
     double x[5];
     double yy[5];
 
-    std::vector<std::pair<double, double>> result;
+    AdamsResult result;
 
     y[0] = y0;
     x[0] = x0;
     yy[0] = 0;
+    result.correctors.emplace_back(x0, y0);
 
-    y[1] = f(x0, y0);
-    y[2] = f(x0 + (h / 2), y0 + (h / 2) * y[1]);
-    y[3] = f(x0 + (h / 2), y0 + (h / 2) * y[2]);
+    std::vector<std::pair<double, double>> rk = RungeKutta4th(x0, y0, h, 3, f);
 
-    for (int k = 0; k <= 3; k++) {
-        x[k] = x[0] + k * h;
-        yy[k] = f(x[k], y[k]);
+    for (int i = 1; i <= 3; i++) {
+        x[i] = rk[i - 1].first;
+        y[i] = rk[i - 1].second;
+        result.correctors.emplace_back(x[i], y[i]);
+    }
+
+    for (int i = 0; i <= 3; i++) {
+        yy[i] = f(x[i], y[i]);
     }
 
     int n = 4;
 
     double yp, yyp;
-    while (n < N) {
+    while (n <= N) {
         x[4] = x[3] + h;
 
         yp = y[3] + h * (55 * yy[3] - 59 * yy[2] + 37 * yy[1] - 9 * yy[0]) / 24;
         yyp = f(x[4], yp);
 
+        result.predictors.emplace_back(x[4], yp);
+
         y[4] = y[3] + h * (9 * yyp + 19 * yy[3] - 5 * yy[2] + yy[1]) / 24;
         yy[4] = f(x[4], y[4]);
 
-        result.emplace_back(x[4], y[4]);
+        result.correctors.emplace_back(x[4], y[4]);
 
         n++;
         x[3] = x[4];
         y[3] = y[4];
-        for (int k = 0; k <= 3; k++) y[k + 1] = y[k];
+        for (int k = 0; k <= 3; k++) yy[k] = yy[k + 1];
     }
 
     return result;
--- main.cpp
+++ main.cpp
@@ -3,27 +3,74 @@
 
 using namespace std;
 
-double f1(double x) {
-    return sqrt(x);
+double f2(double x) {
+    double result;
+    double x_temp = ((x == 0) ? 1e-15 : x);
+    result = sin(x_temp) / x_temp;
+    return result;
 }
 
-double f2(double x) {
-    if (x < 1e-15) return 0;
-    return sin(x) / x;
+double f3(double x, double y) {
+    return y - (2 * x) / y;
 }
 
 int main() {
+    cout << "qinjiushao polynomial" << endl;
+
     cout << (QinJiuShaoPolynomialVal({2, 0, 9, 8, -1, 6, 5}, 8) == 1507906) << endl;
     cout << (QinJiuShaoPolynomialVal({2, 0, 9, 8, -1, 6, 5}, 4) == 27026) << endl;
     cout << (QinJiuShaoPolynomialVal({2, 0, 9, 8, -1, 6, 5}, 3) == 5321) << endl;
     cout << (QinJiuShaoPolynomialVal({2, 0, 9, 8, -1, 6, 5}, -8) == 1106498) << endl;
 
+    cout << "lagrange interpolation" << endl;
+
     cout << (LagrangeInterpolation({{1,9}, {2,6}, {4,0}}, 5) == -3) << endl;
     cout << (LagrangeInterpolation({{1,9}, {2,6}, {4,0}}, 2) == 6) << endl;
     cout << (LagrangeInterpolation({{1,9}, {2,6}, {4,0}}, 8) == -12) << endl;
-    cout << ((LagrangeInterpolation({{2, 3}, {7, 2}, {9, 12}}, -3) - 41.142857142857146) < 0.001) << endl;
+    cout << (abs(LagrangeInterpolation({{2, 3}, {7, 2}, {9, 12}}, -3) - 41.142857142857146) < 0.001) << endl;
     cout << (LagrangeInterpolation({{2, 3}, {7, 2}, {9, 12}}, 9) == 12) << endl;
-    cout << ((LagrangeInterpolation({{2, 3}, {7, 2}, {9, 12}}, -10) - 156.94285714285715) < 0.001) << endl;
+    cout << (abs(LagrangeInterpolation({{2, 3}, {7, 2}, {9, 12}}, -10) - 156.94285714285715) < 0.001) << endl;
 
-    cout << CompositeSimpson(0, 1, 4, f2) << endl;
+    cout << "composite simpson" << endl;
+    cout << (abs(CompositeSimpson(0, 1, 4, f2) - 0.9460869339517937) < 0.001) << endl;
+    cout << (abs(CompositeSimpson(0, 2, 4, f2) - 1.6054969371344003) < 0.001) << endl;
+    cout << (abs(CompositeSimpson(0, 9, 12, f2) - 1.6652433927251669) < 0.001) << endl;
+
+    cout << "rectangle f2 a=0 b=1 step=10" << endl;
+    vector<double> steps = FixedStepRectangle(0, 1, 10, f2);
+    vector<double> stepRefs = {0.9397933, 0.9445135, 0.9456909, 0.9459850, 0.9460596, 0.9460769, 0.9460815, 0.9460827, 0.9460830, 0.9460831};
+    for (int i = 0; i < steps.size(); i++) {
+        cout << (abs(steps[i] - stepRefs[i]) < 0.001) << endl;
+    }
+
+    cout << "Romberg f2 a=0 b=1 e=1e-6" << endl;
+    vector<double> Precise1E6Rectangle = VariableStepRectangle(0, 1, 0.0000001, f2);
+    RombergResult Precise1E6Romberg = Romberg(0, 1, 0.0000001, f2);
+    cout << "rectangle: " << Precise1E6Rectangle.back() << endl;
+    cout << "romberg: " << Precise1E6Romberg.values.back() << endl;
+    cout << "step_rectangle: " << Precise1E6Rectangle.size() << ", step_romberg: " << Precise1E6Romberg.values.size() << ", step_rectangle_binary: " << Precise1E6Romberg.step1 << ", error: " << Precise1E6Romberg.values.back() - Precise1E6Rectangle.back() << endl;
+
+    cout << "ImprovedEuler: f3, y(0)=1, h=0.1, N=10" << endl;
+    vector<pair<double, double>> ImprovedEulerR = ImprovedEuler(0, 1, 0.1, 10, f3);
+    for (auto& p : ImprovedEulerR) {
+        cout << "x: " << p.first << ", y: " << p.second << endl;
+    }
+
+    cout << "RungeKutta4th: f3, y(0)=1, h=0.2, N=5" << endl;
+    vector<pair<double, double>> RK = RungeKutta4th(0, 1, 0.2, 5, f3);
+    for (auto& p : RK) {
+        cout << "x: " << p.first << ", y: " << p.second << endl;
+    }
+
+    cout << "AdamsPredCorrBaseRK4: f3, y(0)=1, h=0.1, N=10" << endl;
+    AdamsResult AdamsPCbRK4 = AdamsPredictorCorrectorBasedOnRK4(0, 1, 0.1, 10, f3);
+    cout << "==============predictors==============" << endl;
+    for (auto& p : AdamsPCbRK4.predictors) {
+        cout << "x: " << p.first << ", y: " << p.second << endl;
+    }
+
+    cout << "==============correctors==============" << endl;
+    for (auto& p : AdamsPCbRK4.correctors) {
+        cout << "x: " << p.first << ", y: " << p.second << endl;
+    }
 }
